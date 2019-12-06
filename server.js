f442114f67e24db0af1bf4ed92531bb1c9a6e53c
Filippo Federico <filippo.federico@comm-it.it>
2019-12-06 19:54:34 +0100

    store in users i dati immessi in /register

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
 
 // variabile dove salvare i dati immessi nel form
-const user = []
+const users = []
 
 app.set('view-engine', 'ejs');
 app.use(express.urlencoded({ extended: false })); // prendere email e password per creare la post nel nostro metodo
@@ -34,10 +35,24 @@ app.get('/register', (req, res) => {
 }) 
 
 // router POST /register
-app.post('/register', (req, res) => {
-    req.body.name,
-    req.body.mail,
-    req.body.password
+app.post('/register', async (req, res) => {
+
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            mail: req.body.mail,
+            password: hashedPassword
+        })
+        // redirect alla pagina login
+        res.redirect('/login')
+        
+    } catch {
+        res.redirect('/register')
+        console.log('ERROR REDIRECT')
+    }
+    console.log('USER: ', users)
 })
 
 app.listen(4000)
